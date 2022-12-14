99289c2d6f8ed010846dd6ec6361ad60782b0a39
gwumex <42099116+gwumex@users.noreply.github.com>
2022-12-14 17:10:24 +0100

    statistics

--- src/App.jsx
+++ src/App.jsx
@@ -16,26 +16,25 @@ import Stats from './components/Stats'
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.paddingX}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar/>
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary `}>
         <div className={`${styles.boxWidth}`}>
           <Hero/>
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary `}>
         <div className={`${styles.boxWidth}`}>
           <Stats/>
         </div>
       </div>
 
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      {/* <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Business/>
           <Billing/>
@@ -43,7 +42,7 @@ const App = () => {
           <Clients/>
           <Footer/>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,17 @@ const Hero = () =>
                 Pament Method
               </h1>
               <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+          </div>
+          <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
 
+          <img src={robot} alt="" className='w-[100%] h-[100%] z-[5]'/>
+          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+          <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient rounded-full' />
+          <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient ' />
+          </div>
+
+          <div className={`ss:hidden ${styles.flexCenter}`}>
+          <GetStarted/>
           </div>
         </section>
 
